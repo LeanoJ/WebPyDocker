ad2a722624d5d226c675d06407852f59ae821921
LeanoJ <leano@schramm22.de>
2025-02-12 12:25:11 +0100

    refactor main.py to simplify routes and add collection endpoints; add requirements.txt for dependencies

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, render_template
-
-import os
+from flask import Flask, render_template
 import psutil
-import platform 
+import platform
 import time
 
 app = Flask(__name__)
 
-@app.route('/') 
-def index(): 
+@app.route('/')
+def index():
     # Get all disk partitions and their usage
     disk_info = []
     for partition in psutil.disk_partitions():
@@ -30,56 +28,34 @@ def index():
         'cpu_percent': psutil.cpu_percent(interval=1),
         'memory_percent': psutil.virtual_memory().percent,
         'disk_info': disk_info,
-        'disk_count': len(disk_info),
-        'boot_time': psutil.boot_time()
+        'disk_count': len(disk_info)
     }
     
     system_information = {
         'platform': platform.system(),
         'machine': platform.machine(),
         'processor': platform.processor(),
-        'system_version': platform.version(),
         'python_version': platform.python_version(),
-        'system_uptime': int((time.time() - psutil.boot_time()) / 60), 
+        'system_uptime': int((time.time() - psutil.boot_time()) / 60),
         'system_uptime_hours': int((time.time() - psutil.boot_time()) / 3600)
     }
     
     return render_template('index.html', 
                          system_health=system_health,
                          system_information=system_information)
- 
-@app.route('/info')
-def info():
-    return render_template('info.html')
 
-@app.route('/cpu')
-def cpu():
-    return render_template('cpu.html')
+@app.route('/collection')
+def collection():
+    return 'Collection!'
 
-@app.route('/memory')
-def memory():
-    return render_template('memory.html')
+@app.route('/collection/<int:collection_id>')
+def collection_id(collection_id):
+    return 'Collection ID: {}'.format(collection_id)
 
-@app.route('/disk')
-def disk():
-    return render_template('disk.html')
+@app.route('/collection/<int:collection_id>/item')
+def collection_item(collection_id):
+    return 'Collection Item!'
 
-@app.route('/network')
-def network():
-    return render_template('network.html')
-
-@app.route('/process')
-def process():
-    return render_template('process.html')
-
-@app.route('/system')
-def system():
-    return render_template('system.html')
-
-@app.route('/get_cpu')
-def get_cpu():
-    cpu = psutil.cpu_percent(interval=1)
-    return str(cpu)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
